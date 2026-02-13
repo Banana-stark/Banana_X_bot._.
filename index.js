c51bc4ccb7ceb3a5359b966609f1c4f5a52b132f
Banana-stark <noobmax002@gmail.com>
2026-02-13 19:07:59 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+import connectToWhatsapp from './Banana_X_bot._./crew.js'
+import handleIncomingMessage from './events/messageHandler.js'
+
+(async() => {
+    await connectToWhatsapp(handleIncomingMessage)
+        console.log('established !')
+})()
